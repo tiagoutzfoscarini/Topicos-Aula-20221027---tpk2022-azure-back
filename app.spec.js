14349f2e3518c4fbcca18c5bf548ae21ae4f5980
rodrigo <rodjle@gmail.com>
2022-10-27 14:01:20 -0300

    added tests

--- app.spec.js
+++ app.spec.js
@@ -1,7 +1,9 @@
 const request=require("supertest");
 const app=require("./app");
+const database=require("./database/database.local");
 
 describe("Conjunto de testes app",()=>{
+
     it ("Testando GetInfo Endpoint",async()=>{
         //cenário
         const esperado="geekxxx";
@@ -11,4 +13,45 @@ describe("Conjunto de testes app",()=>{
         //validação
         expect(res.body.user).toBe(esperado);
     });
+
+    it("Ao salvar um cliente o endpoint deve retornar Ok",async()=>{
+        const databaseSpy = jest.spyOn(database, 'gravarDados');
+        databaseSpy.mockReturnValue(true);
+
+      
+        //cenário
+        const esperado="Ok";
+        //execução
+        const res=await request(app)
+                  .post("/clientes/salvar")
+                  .send({codigo:"",
+                    nome:"236",
+                    endereco:"7889"});
+        
+      
+
+        //validação
+        expect(res.text).toBe(esperado);
+        expect(res.status).toBe(201);
+    });
+
+    it("Ao tentar salvar um cliente e der um erro o endpoint deve retornar  NOk e 401",async()=>{
+        const databaseSpy = jest.spyOn(database, 'gravarDados');
+        databaseSpy.mockReturnValue(false);//gravarDados Sempre Vai Retornar false
+
+      
+        //cenário
+        const esperado="NOk";
+
+        //execução
+        const res=await request(app)
+                  .post("/clientes/salvar")
+                  .send({});
+                  
+      
+          expect(res.status).toBe(401);
+    
+        
+    });
+
 })
