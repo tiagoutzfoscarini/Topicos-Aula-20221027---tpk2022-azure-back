7d023cb5ec54cee32e47a4487a460c4f532eb099
Tiago <52145562+tiagoutzfoscarini@users.noreply.github.com>
2022-10-27 21:20:57 -0300

    Criado operacoesAritmeticas.js

---
+++ OperacoesAritimeticas.js
@@ -0,0 +1,26 @@
+// Soma
+const somar=function(valor1,valor2){
+    return valor1+valor2;
+}
+
+// Subtração
+const subtrair=function(valor1,valor2){
+    return valor1-valor2;
+}
+
+// Multiplicação
+const multiplicar=function(valor1,valor2){
+    return valor1*valor2;
+}
+
+// Divisão
+const dividir=function(valor1,valor2){
+    return valor1/valor2;
+}
+
+module.exports={
+    somar,
+    subtrair,
+    multiplicar,
+    dividir
+}
